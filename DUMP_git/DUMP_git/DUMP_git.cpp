0721536d68e0d29a8cd0d6b49ec49463f51c3f0e
MMijoc <markomijoc18@gmail.com>
2020-11-15 19:14:31 +0100

    commit iz guia

--- DUMP_git/DUMP_git/DUMP_git.cpp
+++ DUMP_git/DUMP_git/DUMP_git.cpp
@@ -6,6 +6,7 @@ int main()
     std::cout << "\n\nHello World! iz c++ jer nemam c#\n";
     std::cout << "\nNista pametno";
     std::cout << "\nOpet malo bezveze";
+    std::cout << "\nJa se nadam zadnji bezvezni dodatak";
 
 }
 
