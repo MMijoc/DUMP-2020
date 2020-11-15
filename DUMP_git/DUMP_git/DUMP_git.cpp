f202de725f3f59aeea78ec891274a5e395174f2c
MMijoc <markomijoc18@gmail.com>
2020-11-15 19:11:41 +0100

    Dodo jos koji bezvezni redak

--- DUMP_git/DUMP_git/DUMP_git.cpp
+++ DUMP_git/DUMP_git/DUMP_git.cpp
@@ -4,6 +4,8 @@
 int main()
 {
     std::cout << "\n\nHello World! iz c++ jer nemam c#\n";
-    std::cout << "\nNista pametno\n";
+    std::cout << "\nNista pametno";
+    std::cout << "\nOpet malo bezveze";
+
 }
 
